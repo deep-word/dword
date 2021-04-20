ae1d1781c4e7f54a92329f2570b69da8c24e8778
Dipam Vasani <dipam44@gmail.com>
2021-04-20 16:01:54 -0400

    documentation changes

--- dword/_nbdev.py
+++ dword/_nbdev.py
@@ -3,10 +3,13 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"URLs": "00_utils.ipynb",
+         "to_hhmmss": "00_utils.ipynb",
+         "to_secs": "00_utils.ipynb",
          "trim_video": "00_utils.ipynb",
          "check_resolution": "00_utils.ipynb",
-         "get_fps": "00_utils.ipynb",
+         "check_fps": "00_utils.ipynb",
          "change_audio_format": "00_utils.ipynb",
+         "trim_audio": "00_utils.ipynb",
          "DeepWord": "01_core.ipynb"}
 
 modules = ["utils.py",
